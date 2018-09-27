5563107385393845564c6f2648f9377512455840
Cameron Latham <cameronlatham@Camerons-MacBook-Pro.local>
2018-09-26 19:07:34 -0500

    Initial commit

---
+++ NYC-Schools-Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYC-Schools-Challenge
+//
+//  Created by Cameron Latham on 9/26/18.
+//  Copyright © 2018 Cameron Latham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
